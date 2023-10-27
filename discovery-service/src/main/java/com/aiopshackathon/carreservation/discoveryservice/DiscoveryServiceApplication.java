dba80a16e2463d47ce8bdea229f6054b91564944
sanjueranga <sanjueranga16@gmail.com>
2023-10-27 10:54:02 +0530

    new test

--- discovery-service/src/main/java/com/aiopshackathon/carreservation/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/aiopshackathon/carreservation/discoveryservice/DiscoveryServiceApplication.java
@@ -11,7 +11,7 @@ public class DiscoveryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 		System.out.println("========================================");
-		System.out.println("Testing Jenkins Job ");
+		System.out.println("Testing Jenkins Job");
 		System.out.println("========================================");
 	}
 
